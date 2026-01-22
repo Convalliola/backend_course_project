006d8e825396b9ea74a1ca45d595de0fbca8d3d1
radilkhanova <radilkhanova@sberbs.ru>
2026-01-22 19:48:54 +0300

    homework1

--- services/moderation.py
+++ services/moderation.py
@@ -1,5 +1,4 @@
 from models.predict import PredictRequest
-from errors import BusinessLogicError
 
 class ModerationService:
     def predict(self, req: PredictRequest) -> bool:
@@ -7,10 +6,6 @@ class ModerationService:
         True есть нарушение
         False нарушений нет
         """
-
-        # # для теста бизнес-ошибки
-        # if req.name.strip().lower() == "__raise__":
-        #     raise BusinessLogicError("Simulated business logic error")
 
         if req.is_verified_seller:
             return False
